b3fd1b7e08b9e9415ebbdb09b8e75fe6031f17d6
Blake Morgan <blakemorgan@gmail.com>
2024-01-23 01:48:36 -0600

    Finished User and user list. Began albums

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -2,6 +2,8 @@ import { GoTrash } from "react-icons/go";
 import Button from "./Button";
 import { deleteUser } from "../store";
 import { useThunk } from "../hooks/useThunk";
+import ExpandablePanel from "./ExpandablePanel";
+import AlbumsList from "./AlbumsList";
 
 
 function UsersListItem({user}) {
@@ -10,18 +12,20 @@ function UsersListItem({user}) {
     const handleClick = () => {
         doDeleteUser(user);
     }
-
-    return <div  className='mb-2 border rounded'>
-                <div className='flex p-2 justify-between items-center cursor-pointer'>
-                    {error && <div>Error Deleting User. </div>}
-                    <div className="flex flex-row items-center justify-between">
-                    <Button className="mr-3" loading={isLoading} onClick={handleClick}>
-                        <GoTrash />
-                    </Button>
-                    {user.name}
-                    </div>
-                </div>
-            </div>
+    
+    const header = <>
+        <Button className="mr-3" loading={isLoading} onClick={handleClick}>
+            <GoTrash />
+        </Button>
+        {error && <div>Error Deleting User. </div>}
+        {user.name}
+    </>
+    return (
+        <ExpandablePanel header={header}>
+            <AlbumsList user={user}/>
+        </ExpandablePanel>
+                
+    )
 }   
 
 export default UsersListItem;
