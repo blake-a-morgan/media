01e9f97c6d5fa3992a6169d2208a22966f04a54c
Blake Morgan <blakemorgan@gmail.com>
2024-01-29 01:18:57 -0600

    Fetch list of albums implemented + Add album btn

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,9 +1,38 @@
-import { useFetchAlbumsQuery } from "../store";
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import Skeleton from '../components/Skeleton';
+import ExpandablePanel from '../components/ExpandablePanel';
+import Button from '../components/Button';
 
 function AlbumsList({user}) {
     const {data, error, isLoading} = useFetchAlbumsQuery(user);
+    const [addAlbum, results] = useAddAlbumMutation();
+
+    const handleAddAlbum = () =>{
+        addAlbum(user);
+    };
+
+    let content;
+
+    if (isLoading){
+        content = <Skeleton  times={3}/>
+    } else if (error){
+        content = <div>Error Loading albums</div>
+    } else{
+        content = data.map(album => {
+            const header = <div>{album.title}</div>;
+
+            return <ExpandablePanel header={header} key={album.id}>
+                List of photos in the album
+            </ExpandablePanel>
+        })
+    }
+
     return <div>
-        Albums for {user.name}
+        <div>
+            Albums for {user.name}
+            <Button onClick={handleAddAlbum}>+ Add Album</Button>
+        </div>
+        <div>{content}</div>
     </div>
 }
 
